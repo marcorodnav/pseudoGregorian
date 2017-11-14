5dd4cf986167163bdbf98e8343dacbdd86c58a67
marcorodnav <marcorodnav@gmail.com>
2017-11-14 14:08:04 -0600

    main modificado

--- src/com/marco/calendar/Main.java
+++ src/com/marco/calendar/Main.java
@@ -4,12 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		Calendario calendario = Calendario.obtenerCalendario();
 		
-		Fecha fecha0 = new Fecha(2017, 11, 14);
-		Fecha fecha1 = new Fecha(2050, 11, 14);
+		Fecha fecha0 = new Fecha(2017, 3, 28);
+		Fecha fecha1 = new Fecha(3050, 3, 28);
 		long inicio = System.currentTimeMillis();
 		Integer dias = calendario.diasEntre(fecha0, fecha1);
 		long fin = System.currentTimeMillis();
 		System.out.println(dias);
-		System.out.println("Tiempo: "+ (fin - inicio));
+		System.out.println("Tiempo: "+ (fin - inicio) + " ms");
 	}
 }
